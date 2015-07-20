85b082de618e7a0f40b99057eda839651153ca68
Artem Sapegin <artem@sapegin.ru>
2015-07-20 23:21:56 +0200

    Add Immutable.js.

--- app/components/MessagesList.js
+++ app/components/MessagesList.js
@@ -1,5 +1,6 @@
 // Author: Artem Sapegin, http://sapegin.me, 2015
 
+import Immutable from 'immutable';
 import Block from 'bem-cn';
 
 let b = new Block('messages-list');
@@ -8,16 +9,16 @@ let PT = React.PropTypes;
 export default React.createClass({
 	displayName: 'MessagesList',
 	propTypes: {
-		messages: PT.array
+		messages: PT.instanceOf(Immutable.List)
 	},
 
 	renderMessages() {
 		let { messages } = this.props;
-		if (messages && messages.length) {
+		if (messages && messages.size) {
 			return messages.map((message) => {
 				return (
 					<div>
-						{message.text}
+						{message.get('text')}
 					</div>
 				);
 			});
--- app/reducers/messages.js
+++ app/reducers/messages.js
@@ -1,33 +1,31 @@
 // Author: Artem Sapegin, http://sapegin.me, 2015
 
+import Immutable from 'immutable';
 import uuid from 'random-uuid-v4';
 import * as types from '../constants/ActionTypes';
 
-let initialState = [
-	{
+let initialState = Immutable.List.of(
+	Immutable.Map({
 		id: 1,
 		text: 'Hello'
-	},
-	{
+	}),
+	Immutable.Map({
 		id: 2,
 		text: 'World'
-	}
-];
+	})
+);
 
 export default function messages(state = initialState, action) {
 	switch (action.type) {
 		case types.ADD_MESSAGE:
 			let id = uuid();
-			return [
-				{
-					id: id,
-					text: action.text
-				},
-				...state
-			];
+			return state.unshift(Immutable.Map({
+				id: id,
+				text: action.text
+			}));
 
 		case types.DELETE_TODO:
-			return state.filter(message => message.id !== action.id);
+			return state.filter(message => message.get('id') !== action.id);
 
 		default:
 			return state;
